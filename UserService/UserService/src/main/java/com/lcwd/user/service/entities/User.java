feb7096d1de7d281fe8a95a2e3105ab477b79520
Anmol Chabbra <anmol.chabbra.1@outlook.com>
2024-07-15 10:15:52 -0400

    Added Rating Entity class, and give the list of ratings reference to user entity class

--- UserService/UserService/src/main/java/com/lcwd/user/service/entities/User.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/entities/User.java
@@ -1,11 +1,11 @@
 package com.lcwd.user.service.entities;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "micro_users")
@@ -28,5 +28,8 @@ public class User {
 
     @Column(name = "ABOUT")
     private String about;
+
+    @Transient
+    private List<Rating> ratings = new ArrayList<>();
 
 }
